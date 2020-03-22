274468b8b2b9f5d933481d0c137f762e2c12c442
dmitryman <dmitryman850@gmail.com>
2020-03-22 22:15:24 +0300

    feat: add happy

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,16 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var happy = sadArray.toCollection(ArrayList())
+        var i = 1
+        while (i  < happy.size-1) {
+            if (happy[i] > happy[i-1]+happy[i+1]) {
+                happy.removeAt(i)
+                if(i!=1) i--
+            }
+        else i++
+        }
+        return happy.toIntArray()
     }
+    //  throw NotImplementedError("Not implemented")
 }
