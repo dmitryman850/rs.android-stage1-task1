1f636dc302f10b2faf5ff140abcc05ffc8d5a977
dmitryman <dmitryman850@gmail.com>
2020-03-29 01:02:56 +0300

    feat: add Task5/1

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -7,19 +7,13 @@ class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        val list = digitString.toInt()
-        println(list)
-        val list2 = "2882"
-//        var i = 0
-//        for (i in 0 until n) {
-//            if (k == 1) {
-//                list[2] = '8'
-//            }
-//            println(i)
-//        }
-//        println(list)
-//        return list.toString()
-        return list2.toString()
-            throw NotImplementedError("Not implemented")
+        val result = digitString.toCharArray()
+        println(result)
+        result[2] = '8'
+        println(result)
+        val a1 = result.joinToString("")
+        println(a1)
+        return a1
+           // throw NotImplementedError("Not implemented")
     }
 }
