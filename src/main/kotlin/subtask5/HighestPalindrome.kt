e5c85506a19455618a84a5a35e639e0dc864ff75
dmitryman <dmitryman850@gmail.com>
2020-03-29 16:30:21 +0300

    feat: add Task5

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,19 +1,56 @@
 package subtask5
 
 import java.util.ArrayList
-import kotlin.system.exitProcess
 
 class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        val result = digitString.toCharArray()
-        println(result)
-        result[2] = '8'
-        println(result)
-        val a1 = result.joinToString("")
-        println(a1)
-        return a1
-           // throw NotImplementedError("Not implemented")
+        var list: ArrayList<Int> = ArrayList()
+        for (char in digitString.toCharArray()) {
+            list.add(char.toInt() - '0'.toInt())
+        }
+        var change = 0
+        var i = 0
+        var max: Int
+        for (i in 0 until n / 2) {
+            if (list[i] !== list[n - 1 - i]) {
+                change++
+            }
+        }
+        println("a равно $change")
+
+        when (true) {
+            k == change -> {
+                for (i in 0 until n / 2) {
+                    if (list[i] != list[n - 1 - i]) {
+                        if (list[i] > list[n - 1 - i])
+                            list[n - 1 - i] = list[i]
+                        else list[i] = list[n - 1 - i]
+                    }
+                }
+            }
+            k < change -> {
+                return "-1"
+            }
+            k > change -> {
+                for(i in 0 until change) {
+                    list[i] = 9
+                    list[n-1-i]
+                }
+                for (i in 0 until n / 2) {
+                        if (list[i] != list[n - 1 - i]) {
+                            max = maxOf(list[i], list[n - 1 - i])
+                            if (list[i] != max)
+                                list[i] = list[n - 1 - i]
+                                else list[n-1-i] = list[i]
+                            }
+                        }
+                    }
+                }
+            println(list)
+                return list.joinToString("")
+            throw NotImplementedError("Not implemented")
     }
 }
+
